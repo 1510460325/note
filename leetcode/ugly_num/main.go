76c6806d9ee20b4b28c8688908f535bcf4d59413
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-08-09 22:53:38 +0800

    feat: add nthSuperUglyNumber

--- leetcode/ugly_num/main.go
+++ leetcode/ugly_num/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
-	fmt.Println(nthUglyNumber(10))
+	//fmt.Println(nthUglyNumber(10))
+	fmt.Println(nthSuperUglyNumber(12, []int{2, 7, 13, 19}))
 }
 
 func nthUglyNumber(n int) int {
@@ -31,4 +35,32 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func nthSuperUglyNumber(n int, primes []int) int {
+	if n == 1 {
+		return 1
+	}
+	sort.Ints(primes)
+	points := make([]int, len(primes))
+	for i := range primes {
+		points[i] = 0
+	}
+	dp := make([]int, n)
+	dp[0] = 1
+	for i := 1; i < n; i++ {
+		minIndex := 0
+		for j := range primes {
+			for primes[j] * dp[points[j]] <= dp[i-1] {
+				points[j]++
+			}
+			now := primes[j] * dp[points[j]]
+			if now < primes[minIndex] * dp[points[minIndex]] {
+				minIndex = j
+			}
+		}
+		dp[i] = primes[minIndex] * dp[points[minIndex]]
+		points[minIndex]++
+	}
+	return dp[n-1]
 }
