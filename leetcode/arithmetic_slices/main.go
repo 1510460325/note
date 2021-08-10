ec360d026c52e5bf9c9063ca6236b85147478893
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-08-10 22:07:02 +0800

    feat: add numberOfArithmeticSlices

---
+++ leetcode/arithmetic_slices/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(numberOfArithmeticSlices([]int{1, 2, 3, 5, 7}))
+}
+
+func numberOfArithmeticSlices(nums []int) (ans int) {
+	if len(nums) < 3 {
+		return 0
+	}
+	s := 0
+	doCount := func(a int) int {
+		return (a - 2) * (a - 1) / 2
+	}
+	for i := 2; i < len(nums); i++ {
+		if nums[i]-nums[i-1] != nums[i-1]-nums[i-2] {
+			if i-s >= 3 {
+				ans += doCount(i - s)
+				fmt.Printf("once: %v, s: %v, i: %v\n", doCount(i-s), s, i)
+			}
+			s = i - 1
+		} else if i == len(nums)-1 {
+			if i-s+1 >= 3 {
+				ans += doCount(i - s + 1)
+				fmt.Printf("last: %v, s:%v, i: %v\n", doCount(i-s+1), s, i)
+			}
+		}
+	}
+	return ans
+}
