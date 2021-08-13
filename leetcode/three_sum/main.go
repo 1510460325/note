fd8fc978c05d0bb66d72780538bb045c1b730759
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-08-13 22:10:28 +0800

    feat: three sum

--- leetcode/three_sum/main.go
+++ leetcode/three_sum/main.go
@@ -7,6 +7,7 @@ import (
 
 func main() {
 	fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1))
+	fmt.Println(threeSum([]int{-2, 0, 0, 2, 2}))
 }
 
 func threeSumClosest(nums []int, target int) (ans int) {
@@ -17,7 +18,7 @@ func threeSumClosest(nums []int, target int) (ans int) {
 		j, k := i+1, len(nums)-1
 		sum := target - nums[i]
 		for j < k {
-			if abs(ans-target) > abs(nums[j]+nums[k]+nums[i] - target) {
+			if abs(ans-target) > abs(nums[j]+nums[k]+nums[i]-target) {
 				ans = nums[j] + nums[k] + nums[i]
 			}
 			if nums[j]+nums[k] > sum {
@@ -37,4 +38,33 @@ func abs(a int) int {
 		return a * -1
 	}
 	return a
+}
+
+func threeSum(nums []int) (ans [][]int) {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		j, k := i+1, len(nums)-1
+		target := nums[i] * -1
+		for j < k {
+			if nums[j]+nums[k] == target {
+				ans = append(ans, []int{nums[i], nums[j], nums[k]})
+				j++
+				for j < len(nums) && nums[j] == nums[j-1] {
+					j++
+				}
+				k--
+				for k > i && nums[k] == nums[k+1] {
+					k--
+				}
+			} else if nums[j]+nums[k] < target {
+				j++
+			} else {
+				k--
+			}
+		}
+	}
+	return ans
 }
