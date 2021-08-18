1d986f99153c77901c677fa948d14481f4f709c3
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-08-18 19:29:06 +0800

    feat: add checkRecordA

--- leetcode/check_records/main.go
+++ leetcode/check_records/main.go
@@ -1,7 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(checkRecordA(10101))
 }
 
 func checkRecord(s string) bool {
@@ -22,4 +24,45 @@ func checkRecord(s string) bool {
 		}
 	}
 	return true
+}
+
+// 552. 学生出勤记录 II
+func checkRecordA(n int) (ans int) {
+	if n == 0 {
+		return 0
+	}
+	const mod int = 1e9 + 7
+	dp := make([][][]int, n)
+	for i := range dp {
+		dp[i] = make([][]int, 2)
+		dp[i][0] = make([]int, 3)
+		dp[i][1] = make([]int, 3)
+	}
+	dp[0][1][0] = 1
+	dp[0][0][1] = 1
+	dp[0][0][0] = 1
+	for i := 1; i < n; i++ {
+		// Absent
+		for k := 0; k <= 2; k++ {
+			dp[i][1][0] = (dp[i][1][0] + dp[i-1][0][k]) % mod
+		}
+		// Present
+		for j := 0; j < 2; j++ {
+			for k := 0; k <= 2; k++ {
+				dp[i][j][0] = (dp[i][j][0] + dp[i-1][j][k]) % mod
+			}
+		}
+		// Late
+		for j := 0; j < 2; j++ {
+			for k := 1; k <= 2; k++ {
+				dp[i][j][k] = (dp[i][j][k] + dp[i-1][j][k-1]) % mod
+			}
+		}
+	}
+	for j := 0; j < 2; j++ {
+		for k := 0; k < 3; k++ {
+			ans = (ans + dp[n-1][j][k]) % mod
+		}
+	}
+	return ans
 }
