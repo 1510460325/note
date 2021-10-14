a00cd0535d18316e9955cf67604f8be0346caf9c
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-10-14 21:31:43 +0800

    feat: add peakIndexInMountainArray

---
+++ leetcode/simulation/peakIndexInMountainArray/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+}
+func peakIndexInMountainArray(arr []int) (ans int) {
+	for i := 1; i < len(arr)-1; i++ {
+		if arr[i] > arr[i-1] && arr[i] > arr[i+1] {
+			ans++
+		}
+	}
+	return ans
+}
