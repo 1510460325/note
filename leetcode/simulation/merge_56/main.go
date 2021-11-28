603a49a84496eedf67297350a72f64159d5c8de7
wangzhengyu.wzy <wangzhengyu.wzy@outlook.com>
2021-11-28 16:44:49 +0800

    feat: add merge

---
+++ leetcode/simulation/merge_56/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println(merge([][]int{
+		{1, 4},
+		{4, 5},
+	}))
+}
+
+func merge(intervals [][]int) (ans [][]int) {
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+	for _, item := range intervals {
+		if len(ans) == 0 {
+			ans = append(ans, item)
+			continue
+		}
+		pre := ans[len(ans)-1]
+		if pre[1] < item[0] {
+			ans = append(ans, item)
+			continue
+		}
+		pre[1] = max(pre[1], item[1])
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
